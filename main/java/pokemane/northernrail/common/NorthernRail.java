357c5b08e7baec551a4175162c3c542647577f78
Jack Cravener <j.d.cravener@gmail.com>
2014-02-15 18:35:21 -0500

    Added packet handling stuff

--- main/java/pokemane/northernrail/common/NorthernRail.java
+++ main/java/pokemane/northernrail/common/NorthernRail.java
@@ -3,6 +3,7 @@ package pokemane.northernrail.common;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import org.omg.PortableInterceptor.ServerRequestInfo;
 
 /**
  * Created by pokemane on 2/15/14.
@@ -20,5 +21,14 @@ public class NorthernRail {
         }
     };
 
+    public static final String TEXTURE_NAME_PREFIX = "northernrail:";
 
+    public static final String TEXTURE_DIRECTORY = RESOURCE_PATH + "textures/";
+    public static final String GUI_DIRECTORY = TEXTURE_NAME_PREFIX + "textures/gui/";
+    public static final String BLOCK_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "blocks/";
+    public static final String ITEM_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "items/";
+    public static final String MODEL_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "models/";
+
+    public static final String LANGUAGE_PATH = RESOURCE_PATH + "languages/";
+    private static final String[] LANGUAGES_SUPPORTED = new String[] { "en_US" };
 }
--- main/java/pokemane/northernrail/common/NorthernRailLoader.java
+++ main/java/pokemane/northernrail/common/NorthernRailLoader.java
@@ -1,7 +1,12 @@
 package pokemane.northernrail.common;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import pokemane.northernrail.util.network.packet.PacketPipeline;
 
 /**
  * Created by pokemane on 2/15/14.
@@ -9,6 +14,28 @@ import cpw.mods.fml.common.network.NetworkCheckHandler;
 
 @Mod(modid = NorthernRail.CHANNEL, name = NorthernRail.MODNAME, version = NRConfig.VERSION, acceptedMinecraftVersions = "[1.7.2,)")
 
+
 public class NorthernRailLoader {
+
+    @Instance(NorthernRail.CHANNEL)
+    public static NorthernRail instance;
+
+    public static final PacketPipeline packetPipeline = new PacketPipeline();
+
+    @EventHandler
+    public void load(FMLInitializationEvent initializationEvent){
+        packetPipeline.initalize();
+
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent preInitializationEvent){
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent postInitializationEvent){
+        packetPipeline.postInitialize();
+    }
 
 }
